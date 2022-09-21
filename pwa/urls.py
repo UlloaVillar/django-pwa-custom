2f25e1a5272e0de8110252f29899868ca4d8c6bf
UlloaVillar <89803281+UlloaVillar@users.noreply.github.com>
2022-09-21 15:52:04 +0200

    Changed manifesto URL

--- pwa/urls.py
+++ pwa/urls.py
@@ -5,6 +5,6 @@ from .views import manifest, service_worker, offline
 # Serve up serviceworker.js and manifest.json at the root
 urlpatterns = [
     re_path(r'^serviceworker\.js$', service_worker, name='serviceworker'),
-    re_path(r'^manifest\.json$', manifest, name='manifest'),
+    re_path(r'^pwa/manifest\.json$', manifest, name='manifest'),
     re_path('^offline/$', offline, name='offline')
 ]
